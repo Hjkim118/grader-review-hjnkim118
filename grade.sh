949cc77ebed96be6bb26977420686ccfe1503d2f
Hojoon118 <hok013@ucsd.edu>
2023-11-27 11:02:16 -0800

    edit2

--- grade.sh
+++ grade.sh
@@ -8,20 +8,28 @@ mkdir grading-area
 git clone https://github.com/ucsd-cse15l-f22/list-methods-lab3 student-submission
 echo 'Finished cloning'
 
+cd /Users/hojoon/Desktop/list-examples-grader/student-submission
+
 if [[ ! -f "ListExamples.java" ]]
 then
     echo "No file submitted"
     exit
 fi
 
+cd /Users/hojoon/Desktop/list-examples-grader
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 cp student-submission/ListExamples.java ./grading-area
-javac ./grading-area/ListExamples.java TestListExamples.java
+cp /Users/hojoon/Desktop/list-examples-grader/TestListExamples.java ./grading-area
 
-if[[$? -ne 0]]
-then
+cd /Users/hojoon/Desktop/list-examples-grader/grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+if [ $? -ne 0 ]; then
     echo "Fail"
 fi
 
