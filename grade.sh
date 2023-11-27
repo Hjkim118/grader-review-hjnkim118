a9aa0c53679803a6d324087ea415d95febd3c551
Hojoon118 <hok013@ucsd.edu>
2023-11-27 10:08:33 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,12 +5,25 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone https://github.com/ucsd-cse15l-f22/list-methods-lab3 student-submission
 echo 'Finished cloning'
 
+if [[ ! -f "ListExamples.java" ]]
+then
+    echo "No file submitted"
+    exit
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+
+cp student-submission/ListExamples.java ./grading-area
+javac ./grading-area/ListExamples.java TestListExamples.java
+
+if[[$? -ne 0]]
+then
+    echo "Fail"
+fi
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
